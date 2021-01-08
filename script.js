144352fa8fc1798cf55c6f24ab85b07cd398b41f
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-01-07 17:53:35 -0800

    added containers for search input, current weather, and 5day

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 var searchButton = $(".searchButton");
 
 var apiKey = "b8ecb570e32c2e5042581abd004b71bb";
+
+// Forloop for the data 
+for (var i = 0; i < localStorage.length; i++) {
+    var city = localStorage.getItem(i);
+    var cityName = $(".list-group").addClass("list-group-item");
+    cityName.append("<li>" + city + "</li>");
+}
+// Key count and search button
+var keyCount = 0;
+searchButton.click(function () {
+
+    var searchInput = $(".searchInput").val();
+
+    // Variables for current and five day weather
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
